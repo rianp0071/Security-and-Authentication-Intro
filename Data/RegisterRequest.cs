35566fbf63240ae87d5ab652301329ebb0b33d4f
rianp0071 <110142809+rianp0071@users.noreply.github.com>
2025-04-08 23:38:54 -0500

    Add files via upload

---
+++ Data/RegisterRequest.cs
@@ -0,0 +1,9 @@
+namespace MyApp.Data
+{
+    public class RegisterRequest
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string Role { get; set; }
+    }
+}
